2238f1d3d0c0d44985df662a3d5f41f94f44ded9
JamisonBice <89953910+JamisonBice@users.noreply.github.com>
2021-11-17 19:19:07 -0600

    Initial commit

---
+++ src/test/java/petReg/PetRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package petReg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
